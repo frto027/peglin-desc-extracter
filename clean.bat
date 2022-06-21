01778f1b80c93fa20050a591cb01cdefaa179173
frto027 <602706150@qq.com>
2022-06-21 21:18:00 +0800

    keyword support

--- clean.bat
+++ clean.bat
@@ -1,4 +1,1 @@
-del buffers\orb_buffer.json
-del buffers\guid_map.json
-del buffers\guid_map_css.json
-del buffers\relic_file_sets.json
+del buffers\*.json
--- orb.py
+++ orb.py
@@ -17,34 +17,7 @@ I2Language = PROJECT_PATH / 'Assets' / 'Resources' / 'I2Languages.asset'
 ORBS_FOLDER = PROJECT_PATH / 'Assets' / 'Resources'/ 'prefabs' / 'orbs'
 
 orb_sprite_height_limit = 32
-
-term_maps = dict()
-
 language_id = 8
-
-with I2Language.open('r', encoding='utf8') as f:
-    obj = yaml.load(f,Loader=yaml.BaseLoader)
-    terms = obj["MonoBehaviour"]["mSource"]["mTerms"]
-    for term in terms:
-        term_maps[term["Term"]] = term["Languages"]
-
-def get_translate(index):
-    d = term_maps[index]
-    return d[language_id] or d[0]
-
-def parse_desc(desc, dict = {}):
-    def rep(x):
-        t = x[1]
-        if not t in dict:
-            print("warning: unknwon variable " + t)
-            return ""
-        return dict[t]
-    desc = re.sub(r"\<style=([_a-zA-Z0-9]+)\>", r'<span class="pg_style_\1">', desc)
-    desc = re.sub(r'\</style\>', '</span>', desc)
-    desc = re.sub(r'\<sprite name="([_A-Za-z0-9]+)"?\>', r'<div class="pg_sprite pg_sprite_\1"></div>', desc)
-    desc = re.sub(r'\{\[([a-zA-Z0-9_]+)\]\}', rep, desc)
-    desc = desc.replace('\n','<br/>', -1)
-    return desc
 
 def buffered(path, default = None):
     path = pathlib.Path('buffers') / path
@@ -59,15 +32,50 @@ def buffer_save(path, value):
     with path.open('w',encoding='utf8') as f:
         f.write(json.dumps(value))
 
+term_maps = buffered('term_maps.json')
+if not term_maps:
+    print('generate term_maps...')
+    term_maps = dict()
+    with I2Language.open('r', encoding='utf8') as f:
+        obj = yaml.load(f,Loader=yaml.BaseLoader)
+        terms = obj["MonoBehaviour"]["mSource"]["mTerms"]
+        for term in terms:
+            term_maps[term["Term"]] = term["Languages"]
+    buffer_save('term_maps.json',term_maps)
+
+
+def get_translate(index):
+    d = term_maps[index]
+    return d[language_id] or d[0]
+
+def parse_desc(desc, dict = {}, keyword_out = None):
+    def rep_var(x):
+        t = x[1]
+        if not t in dict:
+            print("warning: unknwon variable " + t)
+            return ""
+        return dict[t]
+
+    def rep_styles(x):
+        style = x[1]
+        if keyword_out != None:
+            keyword_out.append(style)
+        return f'<span class="pg_style_{style}">'
+    desc = re.sub(r"\<style=([_a-zA-Z0-9]+)\>", rep_styles, desc)
+    desc = re.sub(r'\</style\>', '</span>', desc)
+    desc = re.sub(r'\<sprite name="([_A-Za-z0-9]+)"?\>', r'<div class="pg_sprite pg_sprite_\1"></div>', desc)
+    desc = re.sub(r'\{\[([a-zA-Z0-9_]+)\]\}', rep_var, desc)
+    desc = desc.replace('\n','<br/>', -1)
+    return desc
+
 #orb name -> [orb desc(level1), orb desc(level2), orb desc(level3)]
 
 ################ build guid -> png file name map ##############
 
 guid_map = buffered('guid_map.json')
 generated_sprites_css = buffered('guid_map_css.json', dict())
-if guid_map:
-    pass
-else:
+if not guid_map:
+    print('generate guid_map...')
     guid_map = dict()
     for metafile in (PROJECT_PATH / 'Assets' / 'Texture2D').glob('*.meta'):
         with metafile.open('r',encoding='utf8') as f:
@@ -144,12 +152,9 @@ def GenerateSpriteCss(sprite_obj):
 ################ generate orb info #########################
 
 orb_info = buffered('orb_buffer.json')
-orb_infobuffer = pathlib.Path('buffers/orb_buffer.json')
 
-
-if orb_info:
-    pass
-else:
+if not orb_info:
+    print('generate orb_info...')
     orb_info = []
     for orbfile in ORBS_FOLDER.glob("*.prefab"):
         if not '-Lv' in orbfile.name:
@@ -180,8 +185,9 @@ else:
             # -- outputs
 
             descs = []
+            descs_keywords = []
             for descitem in desc_obj["locDescStrings"]:
-                descs.append({"desc":parse_desc(get_translate('Orbs/' + descitem),args)})
+                descs.append({"desc":parse_desc(get_translate('Orbs/' + descitem),args, descs_keywords)})
 
 
             orb_info.append({
@@ -190,7 +196,8 @@ else:
                 "dmg" : desc_obj["DamagePerPeg"],
                 "cdmg" : desc_obj["CritDamagePerPeg"],
                 "descs":descs,
-                "sprite" : style_name
+                "sprite" : style_name,
+                "keywords" : descs_keywords
             })
 
 
@@ -212,6 +219,7 @@ with (PROJECT_PATH / 'Assets' / 'MonoScript' / 'Assembly-CSharp' / 'Relics' / 'R
 
 RelicSets = buffered('relic_file_sets.json')
 if not RelicSets:
+    print('generate RelicSets...')
     RelicSets = []
     for asset in (PROJECT_PATH / 'Assets' / 'MonoBehaviour').glob('*.asset'):
         with asset.open('r',encoding='utf8') as f:
@@ -249,6 +257,7 @@ def FindMonoBehavioursByGuid(guid):
 
 RelicInfoMap = buffered('relic_info_map.json')
 if not RelicInfoMap:
+    print("generate RelicInfoMap...")
     RelicInfoMap = dict()
     for RelicSet in RelicSets:
         name = RelicSet['m_Name']
@@ -301,11 +310,83 @@ if not RelicInfoMap:
         RelicInfoMap[RelicName]['name'] = RelicName
         RelicInfoMap[RelicName]['sprite'] = style_name
         RelicInfoMap[RelicName]['locName'] = get_translate('Relics/' + locKey + '_name')
-        RelicInfoMap[RelicName]['locDesc'] = parse_desc(get_translate('Relics/' + locKey + '_desc' + descMod),global_local_params)
+        desc_keywords = []
+        RelicInfoMap[RelicName]['locDesc'] = parse_desc(get_translate('Relics/' + locKey + '_desc' + descMod),global_local_params, desc_keywords)
+        RelicInfoMap[RelicName]['keywords'] = desc_keywords
+    buffer_save('relic_info_map.json',RelicInfoMap)
 
 relic_infos = []
 for k in RelicInfoMap:
     relic_infos.append(RelicInfoMap[k])
+
+########## orb keywords ##############
+orb_keywords_map = buffered('orb_keyword_map.json')
+if not orb_keywords_map:
+    print('generate orb_keywords_map...')
+    orb_keywords_map = dict()
+    with (PROJECT_PATH / 'Assets' / 'PrefabInstance' / 'OrbInfoWidget.prefab').open('r', encoding='utf8') as f:
+        for mono in yaml.load_all(config.purge(f), yaml.SafeLoader):
+            if not 'MonoBehaviour' in mono:
+                continue
+            target = mono['MonoBehaviour']
+            if '_keywordDescriptionMapping' in target:
+                for item in target['_keywordDescriptionMapping']:
+                    orb_keywords_map[item['Keyword']] =  parse_desc(get_translate('Statuses/' + item['DescriptionLoc']),global_local_params)
+    buffer_save('orb_keyword_map.json', orb_keywords_map)
+
+
+
+########## relic keywords ###########
+relic_keywords_map = buffered('relic_keyword_map.json')
+if not relic_keywords_map:
+    print('generate relic_keywords_map...')
+    relic_keywords_map = dict()
+    with (PROJECT_PATH / 'Assets' / 'PrefabInstance' / 'RelicInfoWidget.prefab').open('r', encoding='utf8') as f:
+        for mono in yaml.load_all(config.purge(f), yaml.SafeLoader):
+            if not 'MonoBehaviour' in mono:
+                continue
+            target = mono['MonoBehaviour']
+            if '_keywordDescriptionMapping' in target:
+                for item in target['_keywordDescriptionMapping']:
+                    relic_keywords_map[item['Keyword']] =  parse_desc(get_translate('Statuses/' + item['DescriptionLoc']),global_local_params)
+    buffer_save('relic_keyword_map.json',relic_keywords_map)
+
+# inject keywords into orb #
+for orb in orb_info:
+    keywords = []
+    generated_keywords = set()
+    for k in orb['keywords']:
+        if k in orb_keywords_map:
+            if k in generated_keywords:
+                continue
+            generated_keywords.add(k)
+            keywords.append(
+                {'keyword_desc':orb_keywords_map[k],'last?':False}
+            )
+    if len(keywords)>0:
+        keywords[-1]['last?']=True
+    orb['keywords'] = keywords
+    orb['has_keywords'] = len(keywords) > 0
+
+for relic in relic_infos:
+    keywords = []
+    generated_keywords = set()
+    for k in relic['keywords']:
+        if k in relic_keywords_map:
+            if k in generated_keywords:
+                continue
+            generated_keywords.add(k)
+            keywords.append(
+                {
+                    'keyword_desc':relic_keywords_map[k],
+                    'last?':False
+                }
+            )
+    if len(keywords) > 0:
+        keywords[-1]['last?'] = True
+    relic['keywords'] = keywords
+    relic['has_keywords'] = len(keywords) > 0
+
 
 ########## generate sprite css ##############
 
@@ -321,6 +402,8 @@ doc_assets = pathlib.Path('docs') / 'pg_assets'
 
 if not doc_assets.exists():
     doc_assets.mkdir()
+
+print('copy assets...')
 for png in used_assets:
     fr = PROJECT_PATH / 'Assets' / 'Texture2D'/ png
     to = doc_assets / png
@@ -328,14 +411,14 @@ for png in used_assets:
         with to.open('wb') as t:
             t.write(f.read())
 
-
-
+print('generate orb.html...')
 with open('docs/orb.html','w',encoding='utf8') as f:
     with open('templates/orb.html.mustache','r',encoding='utf8') as template:
         f.write(chevron.render(template, {"orbs":orb_info}))
 
-
+print('generate relic.html...')
 with open('docs/relic.html','w',encoding='utf8') as f:
     with open('templates/relic.html.mustache','r',encoding='utf8') as template:
         f.write(chevron.render(template, {"relics":relic_infos}))
 
+print('over')
